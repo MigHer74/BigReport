577a4d5857040d7cbee1df884e783211a05be351
Miguel Hernandez <migher74@outlook.com>
2024-06-02 20:29:54 -0600

    Regions header inserted with id field and name field.

--- screens/regions.py
+++ screens/regions.py
@@ -1,4 +1,4 @@
-from tkinter import Toplevel, Frame
+from tkinter import Toplevel, LabelFrame, Frame, Label, Entry
 
 
 class BigRegions(Toplevel):
@@ -9,3 +9,21 @@ class BigRegions(Toplevel):
     def build(self):
         self.title("Big Report - Regions")
         self.resizable(False, False)
+
+        self.regionsFrame = Frame(self)
+        self.regionsFrame.pack()
+
+        self.headerFrame = LabelFrame(self.regionsFrame)
+        self.headerFrame.grid(row=0, column=0)
+
+        self.idLabel = Label(self.headerFrame, text="Id:")
+        self.idLabel.grid(row=0, column=0)
+
+        self.idEntry = Entry(self.headerFrame, width=5)
+        self.idEntry.grid(row=0, column=1)
+
+        self.nameLabel = Label(self.headerFrame, text="Name:")
+        self.nameLabel.grid(row=0, column=2)
+
+        self.nameEntry = Entry(self.headerFrame, width=25)
+        self.nameEntry.grid(row=0, column=3)
